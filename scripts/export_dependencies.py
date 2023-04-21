3fadf045a036089553eba5a0da6ce3b6b931e9a3
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-04-20 21:58:27 -0400

    chore: update export dependencies script

--- scripts/export_dependencies.py
+++ scripts/export_dependencies.py
@@ -1,8 +1,27 @@
 import shutil
 import subprocess
+from multiprocessing import Pool
 from pathlib import Path
+from typing import List, Optional
 
 import tomli
+
+
+def export_dependencies_from_group(
+    group: Optional[str] = None,
+) -> None:
+    if group is None:
+        # exporting package requirements
+        subprocess.run(
+            "poetry export -o requirements/package.piprequirements --without-hashes --without-urls",  # noqa: E501
+            shell=True,
+        )
+
+    else:
+        subprocess.run(
+            f"poetry export --only={group} -o requirements/{group}.piprequirements --without-hashes --without-urls",  # noqa: E501
+            shell=True,
+        )
 
 
 def main():
@@ -22,20 +41,28 @@ def main():
         pyproject = tomli.load(f)
 
     groups = pyproject["tool"]["poetry"]["group"]
-    groups_names = [name for name in groups]
+    groups_names: List[str] = [name for name in groups]
 
-    # exporting package requirements
-    subprocess.run(
-        "poetry export -o requirements/requirements.txt --without-hashes --without-urls",  # noqa: E501
-        shell=True,
-    )
-
-    # exporting groups requirements
-    for group in groups_names:
-        subprocess.run(
-            f"poetry export --only={group} -o requirements/requirements-{group}.txt --without-hashes --without-urls",  # noqa: E501
-            shell=True,
+    with Pool() as pool:
+        pool.map(
+            export_dependencies_from_group,
+            [None, *groups_names],
         )
+
+    print("Exported all dependencies by group.")
+
+    # # exporting package requirements
+    # subprocess.run(
+    #     "poetry export -o requirements/requirements.txt --without-hashes --without-urls",  # noqa: E501
+    #     shell=True,
+    # )
+
+    # # exporting groups requirements
+    # for group in groups_names:
+    #     subprocess.run(
+    #         f"poetry export --only={group} -o requirements/requirements-{group}.txt --without-hashes --without-urls",  # noqa: E501
+    #         shell=True,
+    #     )
 
 
 if __name__ == "__main__":
