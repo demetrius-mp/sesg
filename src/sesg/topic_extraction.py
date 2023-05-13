18a7e6f8de9aabec59c1f31f32607c11a7137378
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-05-13 15:37:59 -0400

    feat: provide function to correctly create the docs that will be used for topic extraction

--- src/sesg/topic_extraction.py
+++ src/sesg/topic_extraction.py
@@ -1,7 +1,6 @@
 """Topic extraction module.
 
-This module is responsible to provide strategies to extract topics of a set
-of documents.
+This module is responsible to provide topic extraction strategies.
 
 Currently, the only available strategies are
 [LDA (Latent Dirichlet Allocation)](https://www.jmlr.org/papers/volume3/blei03a/blei03a.pdf?ref=https://githubhelp.com),
@@ -9,7 +8,7 @@ and [BERTopic](https://arxiv.org/abs/2203.05794).
 """  # noqa: E501
 
 from enum import Enum
-from typing import Any
+from typing import Any, TypedDict
 
 from sklearn.feature_extraction.text import CountVectorizer
 
@@ -25,6 +24,86 @@ class TopicExtractionStrategy(str, Enum):
 
     lda = "lda"
     bertopic = "bertopic"
+
+
+class DocStudy(TypedDict):
+    """Data container for a study that will be used to generate a doc.
+
+    Attributes:
+        title (str): Title of the study.
+        abstract (str): Abstract of the study.
+        keywords (str): Keywords of the study.
+
+    Examples:
+        >>> study: DocStudy = {
+        ...     "title": "machine learning",
+        ...     "abstract": "machine learning is often used in the industry with the goal of...",
+        ...     "keywords": "machine learning, code smells, defect detection"
+        ... }
+        >>> study
+        {'title': 'machine learning', 'abstract': 'machine learning is often used in the industry with the goal of...', 'keywords': 'machine learning, code smells, defect detection'}
+    """  # noqa: E501
+
+    title: str
+    abstract: str
+    keywords: str
+
+
+def _concat_study_info(
+    study: DocStudy,
+) -> str:
+    r"""Concatenates the information of the study into a string.
+
+    Args:
+        study (DocStudy): Study with title, abstract and keywords.
+
+    Returns:
+        A string with the following format: "{title}\n{abstract}\n{keywords}".
+
+    Examples:
+        >>> study: DocStudy = {
+        ...     "title": "machine learning",
+        ...     "abstract": "machine learning is often used in the industry with the goal of...",
+        ...     "keywords": "machine learning, code smells, defect detection"
+        ... }
+        >>> _concat_study_info(study)
+        'machine learning\nmachine learning is often used in the industry with the goal of...\nmachine learning, code smells, defect detection'
+    """  # noqa: E501
+    title = study["title"]
+    abstract = study["abstract"]
+    keywords = study["keywords"]
+
+    return f"{title}\n{abstract}\n{keywords}"
+
+
+def create_docs(
+    studies_list: list[DocStudy],
+) -> list[str]:
+    r"""Creates a list of documents where each document is a string with the title, abstract and keywords of the study.
+
+    Can be used with [extract_topics_with_lda][sesg.topic_extraction.extract_topics_with_lda] or [extract_topics_with_bertopic][sesg.topic_extraction.extract_topics_with_bertopic].
+
+    Args:
+        studies_list (list[DocStudy]): List of studies with title, abstract and keywords.
+
+    Returns:
+        List of documents.
+
+    Examples:
+        >>> s1: DocStudy = {
+        ...     "title": "machine learning",
+        ...     "abstract": "machine learning is often used in the industry with the goal of...",
+        ...     "keywords": "machine learning, code smells, defect detection"
+        ... }
+        >>> s2: DocStudy = {
+        ...     "title": "artificial intelligence",
+        ...     "abstract": "artificial intelligence is often used in the industry with the goal of...",
+        ...     "keywords": "artificial intelligence, code smells, defect detection"
+        ... }
+        >>> create_docs([s1, s2])
+        ['machine learning\nmachine learning is often used in the industry with the goal of...\nmachine learning, code smells, defect detection', 'artificial intelligence\nartificial intelligence is often used in the industry with the goal of...\nartificial intelligence, code smells, defect detection']
+    """  # noqa: E501
+    return [_concat_study_info(s) for s in studies_list]
 
 
 def extract_topics_with_lda(
