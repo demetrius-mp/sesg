d61786f090f945322a56fa50e15157413801ab4d
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-05-23 10:02:20 -0400

    fix: typo in test name

--- tests/test_scopus/test_multi_client.py
+++ tests/test_scopus/test_multi_client.py
@@ -1,7 +1,7 @@
 from sesg import scopus
 
 
-def test_crete_clients_list_should_return_list_with_3_clients():
+def test_create_clients_list_should_return_list_with_3_clients():
     clients_list = scopus.create_clients_list(
         api_keys_list=["1", "2", "3", "4", "5", "6"],
         n_clients=3,
