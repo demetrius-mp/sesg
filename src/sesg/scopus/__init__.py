5027a5a68bb44aa01d18a8f30e7b092e0e326c8e
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-05-28 13:30:13 -0400

    fix: reexport InvalidStringError from scopus.client

--- src/sesg/scopus/__init__.py
+++ src/sesg/scopus/__init__.py
@@ -5,11 +5,12 @@ efficient client that can fetch as much data as it can, as fast as
 possible.
 """
 
-from .client import OutOfAPIKeysError, ScopusClient, SuccessResponse
+from .client import InvalidStringError, OutOfAPIKeysError, ScopusClient, SuccessResponse
 
 
 __all__ = [
     "ScopusClient",
     "OutOfAPIKeysError",
     "SuccessResponse",
+    "InvalidStringError",
 ]
