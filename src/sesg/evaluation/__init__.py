7a776b09ccb315dad0ed2112afb0f3f953aa2bc7
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-05-31 16:17:06 -0400

    refactor: reexport Study

--- src/sesg/evaluation/__init__.py
+++ src/sesg/evaluation/__init__.py
@@ -4,11 +4,12 @@ This module provides a method to evaluate the performance of a search string
 generated with SeSG.
 """
 
-from .evaluation_factory import EvaluationFactory
+from .evaluation_factory import EvaluationFactory, Study
 from .graph import create_citation_graph
 
 
 __all__ = [
     "EvaluationFactory",
     "create_citation_graph",
+    "Study",
 ]
