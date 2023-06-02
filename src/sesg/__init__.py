fb68834fde07a088fa1731ca2f02b6068f3909f8
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-06-02 00:26:11 -0400

    feat: export all modules in dunder init

--- src/sesg/__init__.py
+++ src/sesg/__init__.py
@@ -1,5 +1,16 @@
 """SeSG (Search String Generator) module."""
 from importlib.metadata import version
 
+from . import evaluation, scopus, search_string, snowballing, topic_extraction
+
+
+__all__ = [
+    "evaluation",
+    "scopus",
+    "search_string",
+    "snowballing",
+    "topic_extraction",
+]
+
 
 __version__ = version(__name__)
