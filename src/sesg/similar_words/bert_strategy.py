65653bc2a60bb3bbc310eb31f9e05ffac18dd927
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-06-28 11:35:53 -0400

    feat: create enrichment text

--- src/sesg/similar_words/bert_strategy.py
+++ src/sesg/similar_words/bert_strategy.py
@@ -1,7 +1,7 @@
 """Generate similar words using BERT."""
 
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, TypedDict
 
 import numpy as np
 import torch
@@ -31,6 +31,26 @@ def check_is_bert_oov_word(
     return word.startswith("##")
 
 
+class EnrichmentStudy(TypedDict):
+    """Data container for a study that will be used to generate an enrichment text.
+
+    Attributes:
+        title (str): Title of the study.
+        abstract (str): Abstract of the study.
+
+    Examples:
+        >>> study: EnrichmentStudy = {
+        ...     "title": "machine learning",
+        ...     "abstract": "machine learning is often used in the industry with the goal of...",
+        ... }
+        >>> study
+        {'title': 'machine learning', 'abstract': 'machine learning is often used in the industry with the goal of...'}
+    """  # noqa: E501
+
+    title: str
+    abstract: str
+
+
 @dataclass
 class BertSimilarWordsGenerator(SimilarWordsGenerator):
     """Generate similar words using BERT.
@@ -40,6 +60,37 @@ class BertSimilarWordsGenerator(SimilarWordsGenerator):
         bert_tokenizer (Any): A BERT tokenizer. For example, `BertTokenizer.from_pretrained("bert-base-uncased")`.
         bert_model (Any): A BERT model. For example, `BertForMaskedLM.from_pretrained("bert-base-uncased")`.
     """  # noqa: E501
+
+    @staticmethod
+    def create_enrichment_text(
+        studies_list: list[EnrichmentStudy],
+    ) -> str:
+        r"""Creates a piece of text that consists of the concatenation of the title and abstract of each study.
+
+        Args:
+            studies_list (list[EnrichmentStudy]): List of studies with title and abstract.
+
+        Returns:
+            The enrichment text.
+
+        Examples:
+            >>> studies = [
+            ...     EnrichmentStudy(title="title1", abstract="abstract1"),
+            ...     EnrichmentStudy(title="title2", abstract="abstract2 \r\ntext"),
+            ...     EnrichmentStudy(title="title3", abstract="abstract3"),
+            ... ]
+            >>> BertSimilarWordsGenerator.create_enrichment_text(studies_list=studies)
+            'title1 abstract1\ntitle2 abstract2 #.text\ntitle3 abstract3\n'
+        """  # noqa: E501
+        enrichment_text = ""
+        for study in studies_list:
+            title = study["title"]
+            abstract = study["abstract"]
+
+            line = f"{title} {abstract}".strip().replace("\r\n", "#.") + "\n"
+            enrichment_text += line
+
+        return enrichment_text
 
     enrichment_text: str
     bert_tokenizer: Any
