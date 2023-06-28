2304c4b3398497e171ec457ed9a6c6c2061fec8e
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-06-28 09:38:05 -0400

    feat: pubyear boundaries setter

--- src/sesg/refactored_search_string/formulation.py
+++ src/sesg/refactored_search_string/formulation.py
@@ -3,6 +3,42 @@
 from typing import Iterable, Literal
 
 
+class InvalidPubyearBoundariesError(ValueError):
+    """The provided pubyear boundaries are invalid."""
+
+
+def set_pub_year_boundaries(
+    string: str,
+    *,
+    min_year: int | None = None,
+    max_year: int | None = None,
+) -> str:
+    """Given a search string, will append `PUBYEAR >` and `PUBYEAR <` boundaries as needed.
+
+    Args:
+        string (str): A search string.
+        min_year (int | None, optional): Minimum year of publication. Defaults to None.
+        max_year (int | None, optional): Maximum year of publication. Defaults to None.
+
+    Returns:
+        A search string with PUBYEAR boundaries.
+
+    Examples:
+        >>> set_pub_year_boundaries(string='title("machine" and "learning")', max_year=2018)
+        'title("machine" and "learning") AND PUBYEAR < 2018'
+    """  # noqa: E501
+    if min_year is not None and max_year is not None and min_year >= max_year:
+        raise InvalidPubyearBoundariesError("Max year must be greater than min year")
+
+    if min_year is not None:
+        string += f" AND PUBYEAR > {min_year}"
+
+    if max_year is not None:
+        string += f" AND PUBYEAR < {max_year}"
+
+    return string
+
+
 def join_tokens_with_operator(
     tokens: Iterable[str],
     operator: Literal["AND", "OR"],
