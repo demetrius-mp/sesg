232ee9ccb20b00df9d0cc1b39e255ec0be527fdb
Demetrius Panovitch <demetrius.mp789@gmail.com>
2023-07-01 00:11:55 -0400

    fix: try to fix slow imports by removing reexport from sesg.__init__.py

--- src/sesg/__init__.py
+++ src/sesg/__init__.py
@@ -1,24 +1,5 @@
 """SeSG (Search String Generator) module."""
 from importlib.metadata import version
 
-from . import (
-    evaluation,
-    scopus,
-    search_string,
-    similar_words,
-    snowballing,
-    topic_extraction,
-)
-
-
-__all__ = [
-    "evaluation",
-    "scopus",
-    "search_string",
-    "snowballing",
-    "topic_extraction",
-    "similar_words",
-]
-
 
 __version__ = version(__name__)
